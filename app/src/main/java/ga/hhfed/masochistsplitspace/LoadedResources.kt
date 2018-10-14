5a5e8e5f4295f7dcbb8e98c1ab47b5df997ba168
samalws <saalws@ctemc.org>
2018-10-14 02:01:55 -0400

    sound system

--- app/src/main/java/ga/hhfed/masochistsplitspace/LoadedResources.kt
+++ app/src/main/java/ga/hhfed/masochistsplitspace/LoadedResources.kt
@@ -3,6 +3,10 @@ package ga.hhfed.masochistsplitspace
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.SoundPool
+import android.os.Build
 
 class LoadedResources(private val res: Resources, private val view: VRView) {
     private fun scaledBmp(r: Int, size: Float): Bitmap {
@@ -15,4 +19,50 @@ class LoadedResources(private val res: Resources, private val view: VRView) {
     val flame: Bitmap = scaledBmp(R.drawable.firefly, 1 / 20f)
     val signMan: Bitmap = scaledBmp(R.drawable.sineman, 1 / 10f)
     val lineMan: Bitmap = scaledBmp(R.drawable.lineman, 1/20f)
+
+    fun playSound(soundID: Int, volume: Float, rate: Float = 1f, loop: Int = 0) {
+        sounds.play(soundID, volume, volume, 0, loop, rate)
+    }
+    val sounds: SoundPool =
+            if (Build.VERSION.SDK_INT >= 21)
+                SoundPool.Builder().setMaxStreams(20).build()
+            else
+                @Suppress("deprecation")
+                SoundPool(20, AudioManager.STREAM_MUSIC, 0)
+    //val dingSound = sounds.load(view.context, R.raw.bell_ding, 0)
+
+    private val soundtracks: List<MediaPlayer> = listOf(
+            /*MediaPlayer.create(context, R.raw.splitspace_theme),
+            MediaPlayer.create(context, R.raw.perilous_battle),
+            MediaPlayer.create(context, R.raw.strange_battle))*/)
+    private var playing = 2
+    private var paused = true
+    fun playTrack(id: Int) {
+        if (playing == id) return
+        playing = id
+        if (paused) resumeTrack()
+        soundtracks.forEach {
+            if (it == soundtracks[id])
+                it.start()
+            else {
+                it.stop()
+                it.prepare()
+            }
+        }
+    }
+    fun pauseTrack() {
+        paused = true
+        soundtracks[playing].pause()
+    }
+    fun resumeTrack() {
+        paused = false
+        soundtracks[playing].start()
+    }
+    init {
+        soundtracks.forEach {
+            it.setVolume(.5f, .5f)
+            it.isLooping = true
+        }
+        resumeTrack()
+    }
 }
